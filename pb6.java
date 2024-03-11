ec2c697047417149b344ca9a214262a45eac5607
Crys <cristian.toanca@emanuel.ro>
2024-03-11 11:03:51 +0200

    6

--- pb6.java
+++ pb6.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class pb6 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        int n = scan.nextInt();
+        int sum = 0;
+        int nrImpar = 1;
+
+        for(int i=0; i<=n; i++)
+        {
+            sum = sum + nrImpar;
+            nrImpar = nrImpar  +2;
+        }
+        System.out.println(sum);
+    }
 }
